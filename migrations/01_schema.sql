5c639a021735aeb66bb582cc81382f443aea7283
Abubakar-Shehu <shehu.lanre@icloud.com>
2025-07-23 17:35:20 -0600

    refactored and corrected some mistakes and inconsistencies in the code

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -5,31 +5,31 @@ DROP TABLE IF EXISTS users CASCADE;
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR NOT NULL,
-  email VARCHAR NOT NULL,
-  password VARCHAR NOT NULL
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255)  NOT NULL,
+  password VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE properties (
   id SERIAL PRIMARY KEY NOT NULL,
   owner_id INTEGER  REFERENCES users(id) ON DELETE CASCADE,
 
-  title VARCHAR NOT NULL,
+  title VARCHAR(255) NOT NULL,
   description TEXT,
-  thumbnail_photo_url VARCHAR NOT NULL,
-  cover_photo_url VARCHAR NOT NULL,
+  thumbnail_photo_url VARCHAR(255)  NOT NULL,
+  cover_photo_url VARCHAR(255)  NOT NULL,
   cost_per_night INTEGER NOT NULL DEFAULT 0,
   parking_spaces INTEGER NOT NULL DEFAULT 0,
   number_of_bathrooms INTEGER NOT NULL DEFAULT 0,
   number_of_bedrooms INTEGER NOT NULL DEFAULT 0,
 
-  country VARCHAR NOT NULL,
-  street VARCHAR NOT NULL,
-  city VARCHAR NOT NULL,
-  province VARCHAR NOT NULL,
-  postcode VARCHAR NOT NULL,
+  country VARCHAR(255)  NOT NULL,
+  street VARCHAR(255)  NOT NULL,
+  city VARCHAR(255)  NOT NULL,
+  province VARCHAR(255)  NOT NULL,
+  post_code VARCHAR(255)  NOT NULL,
 
-  is_active BOOLEAN NOT NULL DEFAULT TRUE
+  active BOOLEAN NOT NULL DEFAULT TRUE
 );
 
 CREATE TABLE reservations (
@@ -40,8 +40,8 @@ CREATE TABLE reservations (
   guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE
 );
 
-CREATE TABLE propert_reviews (
-  id INTEGER NOT NULL,
+CREATE TABLE property_reviews (
+  id SERIAL PRIMARY KEY NOT NULL,
   property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
   guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
